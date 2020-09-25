81a4174271b35871c1c28fcc2f386f4a60fd673e
Selim S. Balci <16270624+selimser@users.noreply.github.com>
2020-09-25 09:30:26 +0100

    XUnit switch, successful VS runner execution (base)

---
+++ DbPeek/DbPeek.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DbPeek.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DbPeek.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("815cb1a4-ac13-40f8-8b14-8887c1a1ff83")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DbPeek/DbPeek.Tests/QueryServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace DbPeek.Tests
+{
+    public class QueryServiceTest
+    {
+
+        [Fact]
+        public void SelimTest()
+        {
+            Assert.Equal(1, 1);
+        }
+    }
+}
