11dc1bbbeb6a88ff59ccc4608b3a00a931b9a135
Selim S. Balci <16270624+selimser@users.noreply.github.com>
2020-09-25 13:02:25 +0100

    QueryService parse tests

--- DbPeek/DbPeek.Tests/QueryServiceTest.cs
+++ DbPeek/DbPeek.Tests/QueryServiceTest.cs
@@ -5,13 +5,87 @@ namespace DbPeek.Tests
 {
     public class QueryServiceTest
     {
+        private const string ExpectedStoredProcedureName = "TestProcedure";
+        private const string ExpectedDefaultSchema = "dbo";
+        private const string ExpectedCustomSchema = "api";
 
         [Fact]
         public void SelimTest()
         {
             Assert.Equal(1, 1);
+        }
 
-            
+        [Theory]
+        [InlineData("dbo.TestProcedure")]
+        [InlineData("[dbo].[TestProcedure]")]
+        public void QueryService_ValidSpName_CorrectSplit(string input)
+        {
+            var result = QueryService.ParseStoredProcedureName(input);
+
+            Assert.Equal(ExpectedDefaultSchema, result.Item1);
+            Assert.Equal(ExpectedStoredProcedureName, result.Item2);
+
+        }
+
+        [Theory]
+        [InlineData("TestProcedure")]
+        [InlineData("[TestProcedure]")]
+        public void QueryService_SpNameWithoutSchema_DefaultDboPrepended(string input)
+        {
+            var result = QueryService.ParseStoredProcedureName(input);
+
+            Assert.Equal(ExpectedDefaultSchema, result.Item1);
+            Assert.Equal(ExpectedStoredProcedureName, result.Item2);
+        }
+
+        [Theory]
+        [InlineData("[dbo.TestProcedure")]
+        [InlineData("dbo].TestProcedure")]
+        [InlineData("[dbo].TestProcedure")]
+        [InlineData("dbo.[TestProcedure")]
+        [InlineData("dbo.TestProcedure]")]
+        [InlineData("dbo.[TestProcedure]")]
+        public void QueryService_IncorrectBracketPlacementWithDefaultSchema_CorrectFormatting(string input)
+        {
+            var result = QueryService.ParseStoredProcedureName(input);
+
+            Assert.Equal(ExpectedDefaultSchema, result.Item1);
+            Assert.Equal(ExpectedStoredProcedureName, result.Item2);
+        }
+
+        [Theory]
+        [InlineData("api.TestProcedure")]
+        [InlineData("[api].[TestProcedure]")]
+        [InlineData("[api].TestProcedure")]
+        [InlineData("api.[TestProcedure]")]
+        public void QueryService_CustomSchema_CorrectFormatting(string input)
+        {
+            var result = QueryService.ParseStoredProcedureName(input);
+
+            Assert.Equal(ExpectedCustomSchema, result.Item1);
+            Assert.Equal(ExpectedStoredProcedureName, result.Item2);
+        }
+
+        [Theory]
+        [InlineData("[api.TestProcedure")]
+        [InlineData("api].TestProcedure")]
+        [InlineData("[api].TestProcedure")]
+        [InlineData("api.[TestProcedure")]
+        [InlineData("api.TestProcedure]")]
+        [InlineData("api.[TestProcedure]")]
+        public void QueryService_CustomSchemaWithIncorrectBrackets_CorrectFormatting(string input)
+        {
+            var result = QueryService.ParseStoredProcedureName(input);
+
+            Assert.Equal(ExpectedCustomSchema, result.Item1);
+            Assert.Equal(ExpectedStoredProcedureName, result.Item2);
+        }
+
+        [Theory]
+        [InlineData("dbo.Test.Procedure")]
+        public void QueryService_DefaultSchema_NameContainsDots(string input)
+        {
+
         }
     }
 }
