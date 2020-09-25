161498dc328d0911fbf96efbeb54650b71188504
Selim S. Balci <16270624+selimser@users.noreply.github.com>
2020-09-25 19:11:35 +0100

    More tests, also checking for success and error objects of the result.

--- DbPeek/DbPeek.Tests/QueryServiceTest.cs
+++ DbPeek/DbPeek.Tests/QueryServiceTest.cs
@@ -9,33 +9,30 @@ namespace DbPeek.Tests
         private const string ExpectedDefaultSchema = "dbo";
         private const string ExpectedCustomSchema = "api";
 
-        [Fact]
-        public void SelimTest()
-        {
-            Assert.Equal(1, 1);
-        }
-
         [Theory]
         [InlineData("dbo.TestProcedure")]
         [InlineData("[dbo].[TestProcedure]")]
-        public void QueryService_ValidSpName_CorrectSplit(string input)
+        public void QueryService_ParseSpNameWithValidSpName_CorrectFormatting(string input)
         {
             var result = QueryService.ParseStoredProcedureName(input);
 
             Assert.Equal(ExpectedDefaultSchema, result.Schema);
             Assert.Equal(ExpectedStoredProcedureName, result.Name);
-
+            Assert.True(result.Success);
+            Assert.Null(result.Error);
         }
 
         [Theory]
         [InlineData("TestProcedure")]
         [InlineData("[TestProcedure]")]
-        public void QueryService_SpNameWithoutSchema_DefaultDboPrepended(string input)
+        public void QueryService_ParseSpNameWithoutSchema_DefaultSchemaPrepended(string input)
         {
             var result = QueryService.ParseStoredProcedureName(input);
 
             Assert.Equal(ExpectedDefaultSchema, result.Schema);
             Assert.Equal(ExpectedStoredProcedureName, result.Name);
+            Assert.True(result.Success);
+            Assert.Null(result.Error);
         }
 
         [Theory]
@@ -45,12 +42,14 @@ namespace DbPeek.Tests
         [InlineData("dbo.[TestProcedure")]
         [InlineData("dbo.TestProcedure]")]
         [InlineData("dbo.[TestProcedure]")]
-        public void QueryService_IncorrectBracketPlacementWithDefaultSchema_CorrectFormatting(string input)
+        public void QueryService_ParseSpNameWithIncorrectBrackesAndDefaultSchema_CorrectFormatting(string input)
         {
             var result = QueryService.ParseStoredProcedureName(input);
 
             Assert.Equal(ExpectedDefaultSchema, result.Schema);
             Assert.Equal(ExpectedStoredProcedureName, result.Name);
+            Assert.True(result.Success);
+            Assert.Null(result.Error);
         }
 
         [Theory]
@@ -58,12 +57,14 @@ namespace DbPeek.Tests
         [InlineData("[api].[TestProcedure]")]
         [InlineData("[api].TestProcedure")]
         [InlineData("api.[TestProcedure]")]
-        public void QueryService_CustomSchema_CorrectFormatting(string input)
+        public void QueryService_ParseSpNameWithCustomSchema_CorrectFormatting(string input)
         {
             var result = QueryService.ParseStoredProcedureName(input);
 
             Assert.Equal(ExpectedCustomSchema, result.Schema);
             Assert.Equal(ExpectedStoredProcedureName, result.Name);
+            Assert.True(result.Success);
+            Assert.Null(result.Error);
         }
 
         [Theory]
@@ -73,13 +74,73 @@ namespace DbPeek.Tests
         [InlineData("api.[TestProcedure")]
         [InlineData("api.TestProcedure]")]
         [InlineData("api.[TestProcedure]")]
-        public void QueryService_CustomSchemaWithIncorrectBrackets_CorrectFormatting(string input)
+        public void QueryService_ParseSpNameWithCustomSchemaAndIncorrectBrackets_CorrectFormatting(string input)
         {
             var result = QueryService.ParseStoredProcedureName(input);
 
             Assert.Equal(ExpectedCustomSchema, result.Schema);
             Assert.Equal(ExpectedStoredProcedureName, result.Name);
+            Assert.True(result.Success);
+            Assert.Null(result.Error);
         }
 
+        [Theory]
+        [InlineData("[dbo].[TestProcedure] ")]
+        [InlineData(" [dbo].[TestProcedure]")]
+        [InlineData(" [dbo].[TestProcedure] ")]
+        public void QueryService_ParseSpNameWithTrailingWhiteSpace_CorrectFormatting(string input)
+        {
+            var result = QueryService.ParseStoredProcedureName(input);
+
+            Assert.Equal(ExpectedDefaultSchema, result.Schema);
+            Assert.Equal(ExpectedStoredProcedureName, result.Name);
+            Assert.True(result.Success);
+            Assert.Null(result.Error);
+        }
+
+        [Theory]
+        [InlineData("[api].[TestProcedure] ")]
+        [InlineData(" [api].[TestProcedure]")]
+        [InlineData(" [api].[TestProcedure] ")]
+        public void QueryService_ParseSpNameWithTrailingWhiteSpaceAndCustomSchema_CorrectFormatting(string input)
+        {
+            var result = QueryService.ParseStoredProcedureName(input);
+
+            Assert.Equal(ExpectedCustomSchema, result.Schema);
+            Assert.Equal(ExpectedStoredProcedureName, result.Name);
+            Assert.True(result.Success);
+            Assert.Null(result.Error);
+        }
+
+        [Theory]
+        [InlineData("[TestProcedure] ")]
+        [InlineData(" [TestProcedure]")]
+        [InlineData(" [TestProcedure] ")]
+        public void QueryService_ParseSpNameWithTrailingWhiteSpaceWithoutSchema_CorrectFormatting(string input)
+        {
+            var result = QueryService.ParseStoredProcedureName(input);
+
+            Assert.Equal(ExpectedDefaultSchema, result.Schema);
+            Assert.Equal(ExpectedStoredProcedureName, result.Name);
+            Assert.True(result.Success);
+            Assert.Null(result.Error);
+        }
+
+        [Theory]
+        [InlineData("dbo.Test,Procedure")]
+        [InlineData("dbo.Test;Procedure")]
+        [InlineData("dbo.Test:Procedure")]
+        [InlineData("dbo.Test\"Procedure")]
+        [InlineData("dbo.Test/Procedure")]
+        public void QueryService_ParseSpnameWithInvalidCharacters_ReturnsError(string input)
+        {
+            var result = QueryService.ParseStoredProcedureName(input);
+
+            Assert.Null(result.Schema);
+            Assert.Null(result.Name);
+            Assert.False(result.Success);
+            Assert.NotNull(result.Error);
+            Assert.Equal(Models.ErrorCodes.InvalidCharacters, result.Error.ErrorCode);
+        }
     }
 }
